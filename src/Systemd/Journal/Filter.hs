9458fc296fc19281fb08eaa98c08fa2924191bb2
Jiri Marsicek <jiri.marsicek@gmail.com>
2024-01-07 11:43:37 +0100

    filter serialization

--- src/Systemd/Journal/Filter.hs
+++ src/Systemd/Journal/Filter.hs
@@ -3,6 +3,8 @@
 
 module Systemd.Journal.Filter where
 
+import Data.Aeson (FromJSON (parseJSON), (.:))
+import qualified Data.Aeson as A
 import Data.Foldable (foldr')
 import qualified Data.HashMap.Strict as Map (lookup)
 import Data.List.NonEmpty (NonEmpty ((:|)))
@@ -27,6 +29,12 @@ data FieldFilter = RegexFieldFilter {
   rfRegex :: Text
 } deriving (Eq, Show)
 
+instance FromJSON FieldFilter where
+  parseJSON = A.withObject "FieldFilter" $ \o -> do
+    rfField <- o .: "field"
+    rfRegex <- o .: "regex"
+    return RegexFieldFilter {rfField, rfRegex}
+
 matchesFieldFilter :: FieldFilter -> JournalEntry -> Bool
 matchesFieldFilter (RegexFieldFilter { rfField, rfRegex}) entry =
       all (PCRE.matches rfRegex . decodeLatin1) $ Map.lookup (mkJournalField rfField) $ journalEntryFields entry
@@ -35,6 +43,15 @@ data EntryFilter =
     NotEntryFilter EntryFilter
   | AndEntryFilter [FieldFilter] 
   deriving (Eq, Show)
+
+instance FromJSON EntryFilter where
+  parseJSON = A.withObject "EntryFilter" $ \o -> do
+    notFilter <- o .: "not"
+    andFilters <- o .: "and"
+    return $ case (notFilter, andFilters) of
+      (Just nf, Nothing) -> NotEntryFilter nf
+      (Nothing, Just af) -> AndEntryFilter af
+      _ -> error "EntryFilter must have either 'not' or 'and' key"
 
 matchesEntryFilter :: EntryFilter -> JournalEntry -> Bool
 matchesEntryFilter (NotEntryFilter ef) entry = not $ matchesEntryFilter ef entry
